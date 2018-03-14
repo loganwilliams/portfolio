fcb5bf16e223c4db53bdefacaffaeb78434bbff1
Logan Williams <logan.williams@alum.mit.edu>
2018-03-14 11:51:41 -0400

    Build script: pushes to portfolio origin.

--- build.sh
+++ build.sh
@@ -3,17 +3,20 @@
 # commit current repo
 git add .
 git commit -m "$1"
+git push origin master
 
 # build hugo
 hugo
 
-# get last commit 
+# get last commit hash
 lasthash=$(git show -s --format=%h)
 
+# copy build product to website WWW directory
 cp -R public/* ~/Documents/loganwilliams.github.io/
 
 cd ~/Documents/loganwilliams.github.io/
 
+# commit and upload the new build products
 commitmessage="match $lasthash: $1"
 git add .
 git commit -m "$commitmessage"
